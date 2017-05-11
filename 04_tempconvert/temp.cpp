2a5e6c58ceba81fe7c216f8c0ce094fa5c7fda8b
fcarras01 <fcarras01@gmail.com>
2017-05-10 23:32:32 -0400

    (quick edits)

--- 04_tempconvert/temp.cpp
+++ 04_tempconvert/temp.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
 int x, y;
 int main( int argc, char *argv[] ) {
 	x = atoi(argv[1]);
 	y = (x * 9/5) + 32;
-	cout << y << "*F" << endl;
+	cout << x << " Celsius converts to..." <<  y << " Fahrenheit" << endl;
 	return 0;
 }
